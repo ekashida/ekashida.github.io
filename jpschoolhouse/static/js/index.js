2f462eb74fcb7ce1d9bf9e990376be0e86282299
Eugene Kashida <ekashida@gmail.com>
2014-11-21 12:24:26 -0800

    Gallery button UI and click handlers

--- jpschoolhouse/static/js/index.js
+++ jpschoolhouse/static/js/index.js
@@ -2,30 +2,34 @@
 
 (function () {
 
-var images = [];
-images.push({
-  url: "https://c2.staticflickr.com/8/7501/15168369784_746f4bdccd.jpg",
-  caption: "IHC"
-});
-images.push({
-  url: "https://c2.staticflickr.com/6/5603/15167569384_4718386711.jpg",
-  caption: "table and chairs"
-});
-images.push({
-  url: "https://c2.staticflickr.com/8/7535/15170130523_84d8c866ee.jpg",
-  caption: "Autumn's language / Le langage de l'automne"
-});
-images.push({
-  url: "https://c2.staticflickr.com/6/5611/15168342233_4afb5dba5b.jpg",
-  caption: "_MG_2362-dualiso-dualiso"
-});
-images.push({
-  url: "https://c1.staticflickr.com/9/8665/15601938637_024dc47cb7.jpg",
-  caption: "winter is coming"
+$('#gallery-cooking-projects').on('click', function () {
+  Strip.show([
+    {
+      url: "https://c2.staticflickr.com/8/7501/15168369784_746f4bdccd.jpg",
+      caption: "IHC"
+    },
+    {
+      url: "https://c2.staticflickr.com/6/5603/15167569384_4718386711.jpg",
+      caption: "table and chairs"
+    }
+  ]);
 });
 
-$('.gallery').on('click', function () {
-  Strip.show(images);
+$('#gallery-backyard-play').on('click', function () {
+  Strip.show([
+    {
+      url: "https://c2.staticflickr.com/8/7535/15170130523_84d8c866ee.jpg",
+      caption: "Autumn's language / Le langage de l'automne"
+    },
+    {
+      url: "https://c2.staticflickr.com/6/5611/15168342233_4afb5dba5b.jpg",
+      caption: "_MG_2362-dualiso-dualiso"
+    },
+    {
+      url: "https://c1.staticflickr.com/9/8665/15601938637_024dc47cb7.jpg",
+      caption: "winter is coming"
+    }
+  ]);
 });
 
 }());
